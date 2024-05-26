6de65e041a1d7a3a838306784bb2b69d60ed5ba3
Andrey Romanov <96443190+Qunexx@users.noreply.github.com>
2024-05-26 19:41:04 +0300

    Добавил роуты профиля

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,59 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\NewsSliderController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentMethodController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [MainController::class, 'index'])->name('index');
 
 Route::post("/logout", function (){
-    return view('index');
+    return view('main.index');
 });
 
 Auth::routes();
 
 
-Route::get('/index', function () {
-    return view('index');
-})->name("index");
-Route::get('/catalog', function () {
-    return view('catalog');
-})->name('catalog');
+Route::get('/main', function () {
+    return view('main.index');
+})->name("main.index");
+
 Route::get('/contact', function () {
-    return view('contact');
-})->name("contact");
+    return view('contact.contact');
+})->name("contact.index");
 Route::get('/about', function () {
-    return view('about');
-})->name('about');
-Route::get('/account', function () {
-    return view('userAccount');
-})->name('account')->middleware('auth');
+    return view('about.about');
+})->name('about.index');
+
 Route::get('/cart', function () {
-    return view('cart');
-})->name('cart')->middleware('auth');
+    return view('cart.cart');
+})->name('cart.index')->middleware('auth');
 Route::get('/delivery', function () {
-    return view('delivery');
-})->name('delivery');
+    return view('delivery.delivery');
+})->name('delivery.index');
 Route::get('/order', function () {
-    return view('order');
-})->name('order')->middleware('auth');
+    return view('order.order');
+})->name('order.index')->middleware('auth');
 
 Route::get('/order/history', function () {
-    return view('orderhistory');
-})->name('order.history')->middleware('auth');
+    return view('orderhistory.orderhistory');
+})->name('order.history.index')->middleware('auth');
 
 Route::get('/favorites', function () {
-    return view('favorites');
-})->name('favorites')->middleware('auth');
+    return view('favorite.favorites');
+})->name('favorites.index')->middleware('auth');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile.index');
+    Route::post('/profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
+});
+
